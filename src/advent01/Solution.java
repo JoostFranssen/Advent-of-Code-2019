b291c3d5e9ab71b8b0356e52fc1ef33ad60c8987
Joost Franssen <jfranssen@sogyo.nl>
2019-12-02 10:21:08 +0100

    Add solution to day 1 puzzle 1

---
+++ src/advent01/Solution.java
@@ -0,0 +1,39 @@
+package advent01;
+
+import java.util.List;
+import java.util.function.Function;
+
+import general.InputReader;
+
+public class Solution {
+	private static final String filename = "src/advent01/input.txt";
+	
+	public static void main(String[] args) {
+		InputReader<Integer> inputReader = new InputReader<>(filename, Integer::valueOf);
+		List<Integer> input = inputReader.getInput();
+		
+		System.out.println(totalRequiredFuel(input, Solution::requiredFuel));
+		System.out.println(totalRequiredFuel(input, Solution::requiredFuelIncludingFuel));
+	}
+	
+	private static int requiredFuel(int mass) {
+		int fuel = mass / 3 - 2;
+		return Math.max(0, fuel);
+	}
+	
+	private static int requiredFuelIncludingFuel(int mass) {
+		int fuel = requiredFuel(mass);
+		
+		int lastFuel = fuel;
+		while(lastFuel != 0) {
+			lastFuel = requiredFuel(lastFuel);
+			fuel += lastFuel;
+		}
+		
+		return fuel;
+	}
+	
+	private static int totalRequiredFuel(List<Integer> masses, Function<Integer, Integer> massToFuel) {
+		return masses.stream().map(massToFuel).reduce((x, y) -> x + y).get();
+	}
+}
