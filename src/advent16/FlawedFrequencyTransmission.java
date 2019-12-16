4946af6864204eb2369f78998019ebcf53b29ca5
Joost Franssen <joost.franssen@hotmail.com>
2019-12-16 22:03:07 +0100

    Add solution day 16 puzzle 2

--- src/advent16/FlawedFrequencyTransmission.java
+++ src/advent16/FlawedFrequencyTransmission.java
@@ -3,34 +3,42 @@ package advent16;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class FlawedFrequencyTransmission {
 	private List<Integer> signal;
 	private List<Integer> basePattern = Arrays.asList(0, 1, 0, -1);
+	private int offset;
 	
 	public FlawedFrequencyTransmission(String input) {
+		this(input, 0);
+	}
+	public FlawedFrequencyTransmission(String input, int offset) {
 		signal = Arrays.asList(input.split("")).stream().map(Integer::valueOf).collect(Collectors.toList());
+		this.offset = offset;
 	}
 	
 	public void performPhase() {
 		List<Integer> newSignal = new ArrayList<>();
 		
 		for(int i = 0; i < signal.size(); i++) {
-			int baseIndex = 0;
-			int baseRepeat = i;
+			int baseIndex = 1;
+			int index = i;
 			int newValue = 0;
-			for(int j = 0; j < signal.size(); j++) {
-				if(baseRepeat <= 0) {
-					baseRepeat = i + 1;
-					baseIndex = (baseIndex + 1) % basePattern.size();
+			
+			while(index < signal.size()) {
+				if(basePattern.get(baseIndex) != 0) {
+					Optional<Integer> partialSum = signal.subList(index, Math.min(index + i + offset + 1, signal.size())).stream().reduce((x, y) -> x + y);
+					if(partialSum.isPresent()) {
+						newValue = newValue + basePattern.get(baseIndex) * partialSum.get();
+//						System.out.println("i=" + i + "; index=" + index + "; basePattern=" + basePattern.get(baseIndex) + "; signalpart=" + signal.subList(index, Math.min(index + i + globalOffset + 1 - offset, signal.size())) + "; partialSum=" + partialSum.get() + "; newValue=" + newValue);
+					}
 				}
-				baseRepeat--;
-				
-				int signalValue = signal.get(j);
-				int baseValue = basePattern.get(baseIndex);
-				newValue += signalValue * baseValue % 10;
+				baseIndex = (baseIndex + 1) % basePattern.size();
+				index += i + offset + 1;
 			}
+			
 			newSignal.add(Math.abs(newValue % 10));
 		}
 		signal = newSignal;
--- src/advent16/Solution.java
+++ src/advent16/Solution.java
@@ -1,5 +1,10 @@
 package advent16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import util.InputReader;
 
 public class Solution {
@@ -19,14 +24,21 @@ public class Solution {
 		
 		//part 2
 		input = input.repeat(10_000);
+		int offset = Integer.parseInt(input.substring(0, 7));
 		
-		fft = new FlawedFrequencyTransmission(input);
+		//to compute the n-th digit in the fft the first n-1 digits do not contribute, so we can ignore the first offset-amount
+		input = input.substring(offset);
+		
+		//input length = 522641, while offset = 5977359; so from the repeated sequence 0, 1, 0, -1, only the 1 appears after skipping the offset
+		//this means that the n-th digit is just the sum mod 10 of the following digits
+		List<Integer> signal = Arrays.asList(input.split("")).stream().map(Integer::valueOf).collect(Collectors.toList());
 		
 		for(int i = 0; i < 100; i++) {
-			fft.performPhase();
+			for(int j = signal.size() - 2; j >= 0; j--) {
+				signal.set(j, (signal.get(j) + signal.get(j + 1)) % 10);
+			}
 		}
 		
-		int skip = Integer.parseInt(fft.toString().substring(0, 7));
-		System.out.println(fft.toString().substring(skip, skip + 8));
+		System.out.println(String.join("", signal.subList(0, 8).stream().map(String::valueOf).toArray(String[]::new)));
 	}
 }
