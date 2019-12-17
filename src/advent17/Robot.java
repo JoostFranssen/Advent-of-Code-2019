fde5322e99fc39b0284873d78d897c9759f7c47b
Joost Franssen <jfranssen@sogyo.nl>
2019-12-17 17:01:49 +0100

    Add begin for day 17

---
+++ src/advent17/Robot.java
@@ -0,0 +1,68 @@
+package advent17;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import intcode.Program;
+
+public class Robot {
+	private Program program;
+	String[] scaffolding;
+	
+	public Robot(List<Long> sourceCode) {
+		program = new Program(sourceCode);
+		run();
+	}
+	
+	private void run() {
+		program.run();
+		StringBuilder sb = new StringBuilder();
+		while(program.hasOutput()) {
+			sb.append((char)program.getNextOutput().intValue());
+		}
+		scaffolding = sb.toString().split("\n");
+	}
+	
+	public List<Point> findIntersections() {
+		List<Point> intersections = new ArrayList<>();
+		for(int y = 0; y < scaffolding.length; y++) {
+			for(int x = 0; x < scaffolding[y].length();x ++) {
+				Point p = new Point(x, y);
+				if(isIntersection(p)) {
+					intersections.add(p);
+				}
+			}
+		}
+		return intersections;
+	}
+	
+	private boolean isIntersection(Point p) {
+		boolean isIntersection = charAtScaffolding(p) == '#';
+		for(int i = -1; i <= 1; i += 2) {
+			try {
+				isIntersection |= charAtScaffolding(new Point(p.x + i, p.y)) == '#';
+			} catch(IndexOutOfBoundsException e) {}
+			try {
+				isIntersection |= charAtScaffolding(new Point(p.x, p.y + i)) == '#';
+			} catch(IndexOutOfBoundsException e) {}
+		}
+		return isIntersection;
+	}
+	
+	private char charAtScaffolding(Point p) {
+		return scaffolding[p.y].charAt(p.x);
+	}
+	
+	private int calcAlignmentParameters(Point intersection) {
+		return intersection.x * intersection.y;
+	}
+	
+	public int sumAllAlignmentParameters() {
+		return findIntersections().stream().map(p -> calcAlignmentParameters(p)).reduce(Integer::sum).get();
+	}
+	
+	public void print() {
+		System.out.println(String.join("\n", scaffolding));
+	}
+}
---
+++ src/advent17/Solution.java
@@ -0,0 +1,23 @@
+package advent17;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import util.InputReader;
+
+public class Solution {
+	private static final String FILENAME = "src/advent17/input.txt";
+	
+	public static void main(String[] args) {
+		InputReader<String> inputReader = new InputReader<>(FILENAME);
+		String input = inputReader.readLines().get(0);
+		List<Long> intcode = Arrays.asList(input.split(",")).stream().map(Long::valueOf).collect(Collectors.toList());
+		
+		//part 1
+		Robot robot = new Robot(intcode);
+		robot.print();
+		System.out.println(robot.findIntersections());
+		System.out.println(robot.sumAllAlignmentParameters());
+	}
+}
